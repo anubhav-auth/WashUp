ebd9da34ce3f45f0a7174e1808c45d1910360d8d
anubhav-auth <anubhavjaiswal2002@gmail.com>
2025-03-20 23:45:35 +0530

    added cloth list

--- app/src/main/java/com/anubhavauth/washup/ClothListThings.kt
+++ app/src/main/java/com/anubhavauth/washup/ClothListThings.kt
@@ -1,45 +1,67 @@
 package com.anubhavauth.washup
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ClothListMenu(modifier: Modifier = Modifier) {
-
+fun ClothListMenu(modifier: Modifier = Modifier, items: List<ClothListContent>) {
+    LazyColumn(modifier = modifier) {
+        items(items) { item ->
+            ClothListItem(item = item)
+        }
+    }
 }
 
 @Composable
 fun ClothListItem(item: ClothListContent) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 21.dp)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 21.dp, vertical = 12.dp)
     ) {
-        Icon(painter = painterResource(item.iconId), contentDescription = "", tint = Color.Red )
-        Column {
+        Image(
+            modifier = Modifier
+                .padding(6.dp)
+                .size(50.dp)
+                .align(Alignment.CenterVertically),
+            painter = painterResource(item.imageId),
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.width(18.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
             Text(item.name)
             Text("${item.quantity} x RS.${item.price}/piece")
         }
-
     }
 }
 
 
 data class ClothListContent(
     val name: String,
-    val iconId: Int,
-    val iconSize: Dp,
+    val imageId: Int,
     val quantity: Int = 0,
     val price: Int
 )
--- app/src/main/java/com/anubhavauth/washup/MainActivity.kt
+++ app/src/main/java/com/anubhavauth/washup/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.anubhavauth.washup.ui.theme.WashUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +16,67 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WashUpTheme {
+
+                val clothList = listOf(
+                    ClothListContent(name = "T-Shirts", imageId = R.drawable.tshirt, price = 8),
+                    ClothListContent(
+                        name = "Shirts (Casual/Formal)",
+                        imageId = R.drawable.shirt,
+                        price = 9
+                    ),
+                    ClothListContent(name = "Jeans/Denim", imageId = R.drawable.jeans, price = 9),
+                    ClothListContent(
+                        name = "Trousers/Chinos",
+                        imageId = R.drawable.trousers,
+                        price = 9
+                    ),
+                    ClothListContent(name = "Shorts", imageId = R.drawable.shorts, price = 8),
+                    ClothListContent(name = "Sweaters", imageId = R.drawable.sweater, price = 9),
+                    ClothListContent(
+                        name = "Hoodies/Jackets",
+                        imageId = R.drawable.jacket,
+                        price = 9
+                    ),
+                    ClothListContent(
+                        name = "Bedsheets",
+                        imageId = R.drawable.bedsheet_single,
+                        price = 15
+                    ),
+                    ClothListContent(
+                        name = "Bedsheets",
+                        imageId = R.drawable.bedsheet_double,
+                        price = 20
+                    ),
+                    ClothListContent(
+                        name = "Pillow Covers",
+                        imageId = R.drawable.pillow_cover,
+                        price = 5
+                    ),
+                    ClothListContent(
+                        name = "Towels",
+                        imageId = R.drawable.towel,
+                        price = 9
+                    ),
+                    ClothListContent(
+                        name = "Handkerchiefs",
+                        imageId = R.drawable.handkerchief,
+                        price = 3
+                    ),
+                    ClothListContent(name = "Socks", imageId = R.drawable.socks, price = 5),
+                    ClothListContent(
+                        name = "Inner wear",
+                        imageId = R.drawable.innerwear,
+                        price = 5
+                    )
+                )
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
-                ) { _ ->
-
+                ) { paddingVal ->
+                    ClothListMenu(
+                        modifier = Modifier.padding(paddingVal),
+                        clothList
+                    )
                 }
             }
         }
